4d398e27214a44a79350ac1c487d6648f44b7bfb
s1120360 <s1120360@dmhs.kh.edu.tw>
2025-12-11 14:10:01 +0800

    作業11

--- main.cpp
+++ main.cpp
@@ -1,47 +1,37 @@
-#include <iostream>
-#include <cstdlib>   // rand(), srand()
-#include <ctime>     // time()
-
-using namespace std;
-
-int main() {
-    const int N = 10;   // 陣列大小
-    int arr[N];
-
-    // ----- 亂數種子 -----
-    srand(time(NULL));
-
-    cout << "原始亂數陣列: ";
-    for (int i = 0; i < N; i++) {
-        arr[i] = rand() % 100;
-        cout << arr[i] << " ";
-    }
-
-    cout << endl;
-
-    // ----- 選擇排序 (Selection Sort) -----
-    for (int i = 0; i < N - 1; i++) {
-        int minIndex = i;
-
-        for (int j = i + 1; j < N; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        // 交換
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-    }
-
-    // ----- 排序後陣列 -----
-    cout << "排序後陣列: ";
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-
+1 #include <iostream>
+2 #include <cstdlib>
+3 #include <ctime>
+4
+5 using namespace std;
+6
+7 int main()
+8 {
+9     int N = 5;
+10    int arr[N];
+11
+12    srand(time(NULL));
+13
+14    cout << "random number: ";
+15    for (int i=0; i<N; i++) {
+16        arr[i] = rand() % 100;
+17        cout << arr[i] << " ";
+18    }
+19
+20    for (int i=0; i<N-1; i++) {
+21        for (int j=0; j<N-1-i; j++) {
+22            if(arr[j] > arr[j+1]) {
+23                int temp = arr[j];
+24                arr[j] = arr[j+1];
+25                arr[j+1] = temp;
+26            }
+27        }
+28    }
+29
+30    cout << "排序後: ";
+31    for (int i=0; i<N; i++) {
+32        cout << arr[i] << " ";
+33    }
+34    cout << endl;
+35
+36    return 0;
+37 }
